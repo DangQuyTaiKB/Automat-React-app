a66bed3a9a9082414305343c23972059623e12a1
Hiep Nguyen <nguyent12@unob.cz>
2022-03-23 00:32:06 +0100

    first commit

---
+++ project/src/components/automat.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Automat extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello world</h1>
+                <svg>
+                
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default Automat;
