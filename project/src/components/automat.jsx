7ff8ea5290da8d0df53b578cf0be8d28b606b409
Hiep Nguyen <nguyent12@unob.cz>
2022-03-23 09:08:33 +0100

    truong hop 3 dinh, tam giac deu

--- project/src/components/automat.jsx
+++ project/src/components/automat.jsx
@@ -8,13 +8,14 @@ class Automat extends Component{
     }
     
     render(){
-        const pozice_3=[[50,50],[100,50],[150,50]];
+        const pozice_3vcholy=[[500,400],[800,400],[650,140]];
         return(
             <div>
                 <h1>Hello world</h1>
-                <p>Pocet vrcholu:{this.state.pocetVrcholu}</p>
-                <svg width="750" height="500">
-                    {pozice_3.map((value,index)=>{
+                
+                <p>Pocet vrcholu: <button className='btn btn-primary btn-sm'>-</button> {this.state.pocetVrcholu} <button className='btn btn-primary btn-sm'>+</button></p>
+                <svg width="1200" height="500">
+                    {pozice_3vcholy.map((value,index)=>{
                         //lap lai circle va text 
                         return <> 
                             <circle cx= {value[0]} cy={value[1]} r="10" stroke="black" stroke-width="1" fill="white"></circle>
--- project/src/index.js
+++ project/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Automat from './components/automat';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
