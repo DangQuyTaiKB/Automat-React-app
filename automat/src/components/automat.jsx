ab863841784e2e5003de813d35905e56960d2e1b
DangQuyTaiKB <dangquytai776617185@gmail.com>
2022-05-19 14:30:19 +0200

    .

--- automat/src/components/automat.jsx
+++ automat/src/components/automat.jsx
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import VratHranu from './hrana'
 import VratVrchol from'./vrchol'
 
+
+
 class Automat extends Component{
     constructor(){
         super();
